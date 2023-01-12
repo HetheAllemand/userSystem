f49c2df95962b639fb983668da98dcbd16ab44d6
HetheAllemand <118325715+HetheAllemand@users.noreply.github.com>
2023-01-11 21:27:41 -0500

    storemanager
    storemanager for class 2
    epic 404 errors

--- scripts/register.js
+++ scripts/register.js
@@ -8,6 +8,49 @@ function User(firstname,lastname,age,gender,phone,payment,color){
     this.color=color;
 }
 
+let inputfName = document.getElementById("txtFName");
+let inputlName = document.getElementById("txtlName");
+let inputAge = document.getElementById("txtAge");
+let inputGenger = document.getElementById("txtGender");
+let inputPhone = document.getElementById("txtPhone");
+let inputPayment = document.getElementById("txtPayment");
+let inputColor = document.getElementById("txtColor");
+
+function validate(theUser){
+    let validation=true;
+    inputfName.classList.remove("alert-error");
+    inputlName.classList.remove("alert-error");
+    inputAge.classList.remove("alert-error");
+    inputGender.classList.remove("alert-error");
+    inputPayment.classList.remove("alert-error");
+
+    if(theUser.fName==""){
+        validation=false;
+        inputfName.classList.add("alert-error");
+    }
+    if(theUser.lName==""){
+        validation=false;
+        inputlName.classList.add("alert-error");
+    }
+    if(theUser.Age==""){
+        validation=false;
+        inputAge.classList.add("alert-error");
+    }
+    if(theUser.Gender==""){
+        validation=false;
+        inputGender.classList.add("alert-error");
+    }
+    if(theUser.Payment==""){
+        validation=false;
+        inputPayment.classList.add("alert-error");
+    }
+
+    return validation;
+}
+
+
+
+
 function register(){
 let inputFname = $("#txtFirstName").val();
 let inputLname = $("#txtLastName").val();
